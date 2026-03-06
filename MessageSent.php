8062e381f4d6186948b665f7ca592d9c9ce462bb
Timothy Nico <136045077+timothynico@users.noreply.github.com>
2026-03-06 13:46:30 +0700

    Create MessageSent.php

---
+++ MessageSent.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\FeedbackMessage;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(FeedbackMessage $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('feedback.' . $this->message->feedback_id),
+        ];
+    }
+
+    /**
+     * Get the data to broadcast.
+     *
+     * @return array
+     */
+    public function broadcastWith(): array
+    {
+        return [
+            'id' => $this->message->id,
+            'message' => $this->message->message,
+            'user_id' => $this->message->user_id,
+            'user_name' => $this->message->user->name ?? 'Unknown',
+            'is_from_customer' => $this->message->is_from_customer,
+            'created_at' => $this->message->created_at->toISOString(),
+        ];
+    }
+}
