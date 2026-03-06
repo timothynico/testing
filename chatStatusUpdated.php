2086be4ea902f661a8a4efd310212e33cd96ce4a
Timothy Nico <136045077+timothynico@users.noreply.github.com>
2026-03-06 13:46:06 +0700

    Create chatStatusUpdated.php

---
+++ chatStatusUpdated.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\ChatRoom;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ChatStatusUpdated implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public ChatRoom $chatRoom;
+
+    public function __construct(ChatRoom $chatRoom)
+    {
+        $this->chatRoom = $chatRoom;
+    }
+
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('chatroom.' . $this->chatRoom->nidchatroom),
+        ];
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'chatRoomId' => $this->chatRoom->nidchatroom,
+            'status'     => $this->chatRoom->cstatus,
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'chat.status.updated';
+    }
+}
